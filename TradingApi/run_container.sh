b607ae2384a1ea309431a50e747450b5224dcf03
ccxAlpha <oskar.bukowski1@gmail.com>
2022-04-07 19:54:55 +0200

    added bash script running container

--- TradingApi/run_container.sh
+++ TradingApi/run_container.sh
@@ -4,13 +4,13 @@
 # $2 --> BUILD NUMBER
 
 
-RUNNING_BEFORE=$(docker info | grep "Running:" | cut -f' ' -f4)
+RUNNING_BEFORE=$(docker info | grep "Running:" | cut -d' ' -f4)
 CONTAINER_NAME="api:$1"
 
 docker build -t "$CONTAINER_NAME" .
 docker run -d "$CONTAINER_NAME"
 
-RUNNING_AFTER=$(docker info | grep "Running:" | cut -f' ' -f4)
+RUNNING_AFTER=$(docker info | grep "Running:" | cut -d' ' -f4)
 
 if [ $RUNNING_AFTER != $RUNNING_BEFORE ]; then
   echo "$2: Unable to run container at $(now)" >> errors.txt
